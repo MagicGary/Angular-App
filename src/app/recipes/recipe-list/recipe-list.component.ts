a8fce20b4233f177cea427339e652f95a4ee2b91
Magic Gary <magicgary@Magics-MacBook-Pro.local>
2018-09-08 19:34:17 -0500

    adds more code into components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'a test', 'https://prods3.imgix.net/images/articles/2017_06/Non-Feature-grilled-mexican-street-corn-recipe-elotes.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=670'),
+    new Recipe('A test recipe', 'a test', 'https://prods3.imgix.net/images/articles/2017_06/Non-Feature-grilled-mexican-street-corn-recipe-elotes.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=670'),
+    new Recipe('A test recipe', 'a test', 'https://prods3.imgix.net/images/articles/2017_06/Non-Feature-grilled-mexican-street-corn-recipe-elotes.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=670')
+  ];
 
   constructor() { }
 
