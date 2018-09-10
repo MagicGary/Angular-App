b5e92700a4219beb875471cbea618e328c4880d5
Magic Gary <magicgary@Magics-MacBook-Pro.local>
2018-09-10 01:53:52 -0500

    adds component interaction and event emitter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    //featureSelected is an event created  using EventEmitter
+    //this @Output() keywords expose this featureSelected Event to its parent's component
+    //to other components so they can listen to this event 
+    @Output() featureSelected = new EventEmitter<string>();
 
+    onSelect(feature: string){
+        //the event is emitted when this onSelect event is triggerd 
+        //this event emits a string called 'feature'
+            //this string feature is passed into onSelected Event
+            //and this featureSelected event emits out this string called feature
+        //this event is being used in the header.component.html 
+        this.featureSelected.emit(feature);
+    }
 }
